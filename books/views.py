9a3770da80871eafaeb7403fc73929af68b1cb87
Sunday James <jamezslim90@gmail.com>
2024-04-06 16:08:36 +0100

    added week6

--- books/views.py
+++ books/views.py
@@ -1,12 +1,12 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
-
+from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from .models import Book
 
-from bookstore_project.settings import MONNIFY_SECRET_KEY, MONNIFY_API_KEY 
+from bookstore_project.settings import MONNIFY_SECRET_KEY, MONNIFY_API_KEY , FLWPUBK_TEST
 
 
 
@@ -16,11 +16,13 @@ class HomePageView(ListView):
     model = Book
     context_object_name = "book_list"
     template_name = "book/home.html"
+    paginate_by = 2
   
     
 class HomeDetailView(LoginRequiredMixin, DetailView):
     model = Book
     template_name= "book/book_detail.html"
+   
     
   
     
@@ -31,7 +33,8 @@ class HomeDetailView(LoginRequiredMixin, DetailView):
         context.update({
                 "book_list": queryset,
                 "MONNIFY_SECRET_KEY": MONNIFY_SECRET_KEY,
-                "MONNIFY_API_KEY": MONNIFY_API_KEY
+                "MONNIFY_API_KEY": MONNIFY_API_KEY,
+                "FLWPUBK_TEST": FLWPUBK_TEST
                 
             })
         return context
@@ -44,7 +47,6 @@ class SearchResultsListView(ListView):
    
     def get_queryset(self): # new
         query = self.request.GET.get("q")
-        return Book.objects.filter(
-            title__icontains= query
-        )
+        
+        return Book.objects.filter(title__icontains= query)
    
