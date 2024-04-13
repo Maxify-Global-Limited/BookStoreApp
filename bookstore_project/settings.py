e322ed506f20fb434f8d092a556109e0b45f5a9a
Sunday James <jamezslim90@gmail.com>
2024-04-13 12:23:56 +0100

    Allowed Host2

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1","bookstoreapp-1.onrender.com/"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "bookstoreapp-1.onrender.com",]
 
 
 # Application definition
